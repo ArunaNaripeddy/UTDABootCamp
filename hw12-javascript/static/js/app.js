fcbf0bb7bf88cfd9c86016ab9c80f256389ee522
aruna <aruna.naripeddy@gmail.com>
2018-07-30 16:52:03 -0500

    Fixed the error

--- hw12-javascript/static/js/app.js
+++ hw12-javascript/static/js/app.js
@@ -1,11 +1,8 @@
-// from data.js
+// Assign the data from `data.js` to a descriptive variable
 var tableData = data;
 
-// Assign the data from `data.js` to a descriptive variable
-var people = data;
-
 // Select the submit button
-var submit = d3.select("#submit");
+var submit = d3.select("#filter-btn");
 
 submit.on("click", function() {
 
@@ -13,35 +10,21 @@ submit.on("click", function() {
   d3.event.preventDefault();
 
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#patient-form-input");
+  var inputElement = d3.select("#datetime");
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
 
-  console.log(inputValue);
-  console.log(people);
+  var filteredData = tableData.filter(sightings => sightings.datetime === inputValue);
 
-  var filteredData = people.filter(person => person.bloodType === inputValue);
+  var tbody = d3.select("#ufo-table-tbody");
 
-  console.log(filteredData);
-
-  // BONUS: Calculate summary statistics for the age field of the filtered data
-
-  // First, create an array with just the age values
-  var ages = filteredData.map(person => person.age);
-
-  // Next, use math.js to calculate the mean, median, mode, var, and std of the ages
-  var mean = math.mean(ages);
-  var median = math.median(ages);
-  var mode = math.mode(ages);
-  var variance = math.var(ages);
-  var standardDeviation = math.std(ages);
-
-  // Finally, add the summary stats to the `ul` tag
-  d3.select(".summary")
-    .append("li").text(`Mean: ${mean}`)
-    .append("li").text(`Median: ${median}`)
-    .append("li").text(`Mode: ${mode}`)
-    .append("li").text(`Variance: ${variance}`)
-    .append("li").text(`Standard Deviation: ${standardDeviation}`);
+  data.forEach((filteredData) => {
+    var row = tbody.append("tr");
+    Object.entries(filteredData).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
 });
+
